0c5de756a365ae4ecc6067d589629dba764b36ef
carlitosvargas <carloscabj@gmail.com>
2026-06-08 12:40:21 -0300

    corrige importacion de middleware que tiraba el servidor

--- src/routes/notificacionRoutes.js
+++ src/routes/notificacionRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { verificarNotificaciones, ejecutarCronRecordatorios } = require('../controllers/notificacionController');
-const authMiddleware = require('../middlewares/authMiddleware');
+const { verificarToken } = require('../middlewares/authMiddleware');
 
 // Endpoint privado (dueño) que hace polling
-router.get('/turnos-proximos', authMiddleware, verificarNotificaciones);
+router.get('/turnos-proximos', verificarToken, verificarNotificaciones);
 
 // Endpoint público para que cron-job.org haga ping cada minuto
 router.get('/cron', ejecutarCronRecordatorios);
